6b1be3e0561a5f364675181dab7b92c8895e5400
Ullrich Schäfer <hi+github@ullrich.is>
2024-12-18 14:20:45 +0100

    APNS production env for TestFlight

--- packages/react-native-push-info/ios/PushInfo.swift
+++ packages/react-native-push-info/ios/PushInfo.swift
@@ -8,8 +8,17 @@ class PushInfo: NSObject {
 
   @objc
   func constantsToExport() -> [String: Any]! {
+#if targetEnvironment(simulator)
+    let apnsEnvironment = "development"
+#else
+    var apnsEnvironment = embeddedProvision?.entitlements.apsEnvironment?.rawValue
+    if apnsEnvironment == nil {
+      let isTestFlight = Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
+      apnsEnvironment = isTestFlight ? "production" : "development"
+    }
+#endif
     return ["bundleId": Bundle.main.bundleIdentifier as Any,
             "teamId": embeddedProvision?.entitlements.teamId as Any,
-            "apnsEnvironment": embeddedProvision?.entitlements.apsEnvironment?.rawValue as Any]
+            "apnsEnvironment": apnsEnvironment as Any]
   }
 }
